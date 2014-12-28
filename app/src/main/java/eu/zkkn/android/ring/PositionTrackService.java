24b947ce3c951d041f7bd37944c4fd8364a70411
Zdeněk Kořán <zkoran@gmail.com>
2014-12-28 17:23:59 +0100

    add switching to silent mode when device is put face down while ringing
    At first, the device has to be face up, so user can see who is calling.

--- app/src/main/java/eu/zkkn/android/ring/PositionTrackService.java
+++ app/src/main/java/eu/zkkn/android/ring/PositionTrackService.java
@@ -8,15 +8,22 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 public class PositionTrackService extends Service implements SensorEventListener {
 
+    private AudioManager mAudioManager;
     private SensorManager mSensorManager;
+    int ringerMode = AudioManager.RINGER_MODE_NORMAL;
     private boolean faceUp = false;
-    //private boolean faceDown = false;
+    private boolean faceDown = false;
+    private boolean muted = false;
+
+    private static final String LOG_TAG = "ring_log";
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -28,11 +35,12 @@ public class PositionTrackService extends Service implements SensorEventListener
     public void onCreate() {
         Toast.makeText(getApplicationContext(), getClass().getName() + ".onCreate()", Toast.LENGTH_SHORT).show();
 
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        ringerMode = mAudioManager.getRingerMode();
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-
     }
 
     @Override
@@ -40,6 +48,8 @@ public class PositionTrackService extends Service implements SensorEventListener
         Toast.makeText(getApplicationContext(), getClass().getName() + ".onDestroy()", Toast.LENGTH_SHORT).show();
 
         mSensorManager.unregisterListener(this);
+        mAudioManager.setRingerMode(ringerMode);
+        Log.d(LOG_TAG, "RINGER MODE RESTORED");
     }
 
     @Override
@@ -48,14 +58,20 @@ public class PositionTrackService extends Service implements SensorEventListener
         int y = Math.round(event.values[1]);
         int z = Math.round(event.values[2]);
 
-
+        // device must by face up at first
         if (!faceUp) {
             faceUp = isFaceUp(x, y, z);
-        } else if (isFaceDown(x, y, z)) {
-            showNotification();
-            stopSelf();
+        } else {
+            if (!faceDown) {
+                faceDown = isFaceDown(x, y, z);
+            }
         }
 
+        if (!muted && faceDown && faceUp) {
+            muted = true;
+            mSensorManager.unregisterListener(this);
+            showNotification();
+        }
     }
 
     @Override
@@ -71,6 +87,10 @@ public class PositionTrackService extends Service implements SensorEventListener
     }
 
     private void showNotification() {
+
+        mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        Log.d(LOG_TAG, "SILENT MODE");
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_launcher)
