72d44bcc6a1bdf283cc02875b10edd1d4c7ca758
Zdeněk Kořán <zkoran@gmail.com>
2014-12-30 22:28:24 +0100

    refactor starting and stopping of service for tracking of device orientation

--- app/src/main/java/eu/zkkn/android/ring/PhoneStateReceiver.java
+++ app/src/main/java/eu/zkkn/android/ring/PhoneStateReceiver.java
@@ -1,6 +1,7 @@
 package eu.zkkn.android.ring;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
@@ -19,19 +20,24 @@ public class PhoneStateReceiver extends BroadcastReceiver {
                 // Device call state: Ringing. A new call arrived and is ringing or waiting. In the latter case, another call is already active.
                 String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 MyLog.l("RINGING; number: " + number);
-                context.startService(new Intent(context, PositionTrackService.class));
-            } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-                // Device call state: Off-hook. At least one call exists that is dialing, active, or on hold, and no calls are ringing or waiting.
-                MyLog.l("OFFHOOK");
-                //TODO: check if service is running
-                context.stopService(new Intent(context, PositionTrackService.class));
-            } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-                // Device call state: No activity.
-                MyLog.l("IDLE");
-                //TODO: see OFFHOOK todo
-                context.stopService(new Intent(context, PositionTrackService.class));
+                ComponentName componentName = context.startService(new Intent(context, PositionTrackService.class));
+                if (componentName == null) {
+                    MyLog.l("PositionTrackService couldn't be started");
+                }
             } else {
-                MyLog.l("UNKNOWN");
+                if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                    // Device call state: Off-hook. At least one call exists that is dialing, active, or on hold, and no calls are ringing or waiting.
+                    MyLog.l("OFFHOOK");
+                } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                    // Device call state: No activity.
+                    MyLog.l("IDLE");
+                } else {
+                    MyLog.l("UNKNOWN");
+                }
+                boolean stopped = context.stopService(new Intent(context, PositionTrackService.class));
+                if (!stopped) {
+                    MyLog.l("PositionTrackService couldn't be stopped");
+                }
             }
 
         }
