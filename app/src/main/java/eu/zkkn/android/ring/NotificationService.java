b21e2c8d011b4f17c3c306f78bcfa84bbb7583c4
Zdeněk Kořán <zkoran@gmail.com>
2015-01-11 13:35:10 +0100

    fix that beep sound is played even though the Silent Mode is active
    Silent mode doesn't affect alarms nor media, so we have to set appropriate stream type, which is Notification. But MediaPlayer.create() doesn't allow it, so we can't use it.

--- app/src/main/java/eu/zkkn/android/ring/NotificationService.java
+++ app/src/main/java/eu/zkkn/android/ring/NotificationService.java
@@ -2,28 +2,20 @@ package eu.zkkn.android.ring;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.provider.CallLog;
+import android.util.Log;
+
+import java.io.IOException;
 
 
-public class NotificationService extends Service {
+public class NotificationService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
 
-    MediaPlayer mPlayer;
-
-    @Override
-    public void onCreate() {
-        mPlayer = MediaPlayer.create(this, R.raw.beep);
-        // TODO: mPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
-        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.release();
-                stopSelf();
-            }
-        });
-    }
+    MediaPlayer mPlayer = null;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -35,15 +27,9 @@ public class NotificationService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         MyLog.l(getClass().getName() + ".onStart()");
         MyLog.l(intent);
-        String[] projection = {CallLog.Calls._ID};
-        Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, projection,
-                CallLog.Calls.NEW + " = 1 AND " + CallLog.Calls.TYPE + " = " + CallLog.Calls.MISSED_TYPE,
-                null, null);
-
-        if (cursor.getCount() > 0) {
-            // TODO: play in a new thread instead of main thread
+        if (hasNewMissedCalls()) {
             MyLog.l("Beeeep!!!");
-            mPlayer.start();
+            playNotificationSound();
         } else {
             MyLog.l("Cancel Alarm");
             NotificationAlarmReceiver.cancelAlarm(this);
@@ -54,8 +40,46 @@ public class NotificationService extends Service {
 
     @Override
     public void onDestroy() {
-        mPlayer.release();
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
         MyLog.l("Beep service destroyed");
     }
 
+    private boolean hasNewMissedCalls() {
+        String[] projection = {CallLog.Calls._ID};
+        Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, projection,
+                CallLog.Calls.NEW + " = 1 AND " + CallLog.Calls.TYPE + " = " + CallLog.Calls.MISSED_TYPE,
+                null, null);
+        MyLog.l("Missed calls: " + cursor.getCount());
+        return cursor.getCount() > 0;
+    }
+
+    private void playNotificationSound() {
+        try {
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.beep);
+            if (afd == null) return;
+            mPlayer = new MediaPlayer();
+            mPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
+            mPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            afd.close();
+            mPlayer.setOnCompletionListener(this);
+            mPlayer.setOnPreparedListener(this);
+            mPlayer.prepareAsync();
+        } catch (IOException e) {
+            Log.d("NotificationService", "MediaPlayer failed", e);
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mp.release();
+        stopSelf();
+    }
 }
